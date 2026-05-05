b82929de7e8424205b31d7afe651adfc5c5d0437
uday-nagar <udaynagar07@gmail.com>
2026-05-05 22:17:02 +0530

    pass by value and refrance syntax

--- functions.cpp
+++ functions.cpp
@@ -10,12 +10,29 @@ void greet(){
 int sum(int a, int b){
     return a+b;
 }
+
+//these are similar to java
+
+
+//
+// pass by reference
+int keepValue(int a){// add & before a to make it pass by reference
+    a+=5;
+    cout<<"inside function "<<a<<endl;
+    return a;
+}
+
+
+
 int main(){
     greet();
     int x, y;
     cin>>x>>y;
     cout<<sum(x, y)<<endl;
+
+
+    int k = 5;
+    cout<<keepValue(k)<<endl; // this will print 10
+    cout<<k<<endl; // this will print 5 because k is passed by value
     return 0;
 }
-
-//these are similar to java
