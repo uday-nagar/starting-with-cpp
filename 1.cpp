cf1330797519bdfde98b0bbcf934ac5d22ff964c
uday-nagar <udaynagar07@gmail.com>
2026-05-05 19:24:15 +0530

    commit no.1

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a ;
+    cin >> a ;
+    cout << "hey " << "std::cout " <<endl ;
+    cout << "this is my name"<< a ;
+    return 0;
+}
